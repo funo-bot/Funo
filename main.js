da5f875e5a9aae91b8d0e3035c70b563ab30fbf9
Dylan <delxhq0@gmail.com>
2018-04-09 00:53:49 +0100

    Rewrote the command loader

--- main.js
+++ main.js
@@ -5,24 +5,30 @@ const moment    = require('moment');
 
 const funo      = new Discord.Client();
 
-const logger    = require('./functions/terminal.js');
+const logger    = require('./functions/terminal');
 
 require('./util/eventLoader')(funo);
 
 funo.commands     = new Discord.Collection();
-funo.aliases      = new Discord.Collection();
 
 fs.readdir('./commands/', (err, files) => {
-    if (err) console.log(err);
-    logger(funo, `Loading ${files.length} commands...`);
-    files.forEach(f => {
-        let props = require(`./commands/${f}`)
-        logger(funo, `Loading command: ${props.help.name}...`);
-        funo.coammnds.set(props.help.name, props)
-        props.conf.aliases.forEach(alias => {
-            funo.aliases.set(alias, props.help.name);
-        });
+    if (err) logger(funo, err);
+
+    let jsfiles = files.filter(f => f.split('.').pop() === 'js');
+    if (jsfiles.length <= 0) {
+        logger(funo, 'No commands to load!');
+        return undefined;
+    }
+
+    logger(funo, `Loading ${jsfiles.length} commands...`);
+
+    jsfiles.forEach((f, i) => {
+        let props = require(`./commands/${f}`);
+        funo.commands.set(props.help.name, props);
+        logger(funo, `${f} command loaded!`);
     });
+
+    logger(funo, 'All commands loaded!');
 });
 
 funo.login(config.token);
