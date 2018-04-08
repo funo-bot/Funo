1cf06df5804b20f4484898446ffcdeb4b899e179
Dylan <delxhq0@gmail.com>
2018-04-08 23:42:00 +0100

    No need for dates

--- functions/terminal.js
+++ functions/terminal.js
@@ -1,4 +1,4 @@
 const moment = require('moment');
 module.exports = async (funo, message) => {
-    console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
+    console.log(`[${moment().format('HH:mm:ss')}] ${message}`);
 };
