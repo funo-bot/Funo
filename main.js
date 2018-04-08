b62c197d06548efc6ea510d40a584cd54d0eab00
Dylan <delxhq0@gmail.com>
2018-04-08 23:38:15 +0100

    Update code for new logger

--- main.js
+++ main.js
@@ -5,26 +5,24 @@ const moment    = require('moment');
 
 const funo      = new Discord.Client();
 
-require('./util/eventLoader')(funo);
+const logger  = require('./functions/terminal.js');
 
-const log = message => {
-    console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
-};
+require('./util/eventLoader')(funo);
 
 funo.commands     = new Discord.Collection();
 funo.aliases      = new Discord.Collection();
 
 fs.readdir('./commands/', (err, files) => {
     if (err) console.log(err);
-    log(`Loading ${files.length} commands...`);
+    logger(funo, `Loading ${files.length} commands...`);
     files.forEach(f => {
         let props = require(`./commands/${f}`)
-        log(`Loading command: ${props.help.name}...`);
+        logger(funo, `Loading command: ${props.help.name}...`);
         funo.coammnds.set(props.help.name, props)
         props.conf.aliases.forEach(alias => {
             funo.aliases.set(alias, props.help.name);
         });
     });
-}) 
+});
 
 funo.login(config.token);
