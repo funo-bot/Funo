58180b2ef5ac8b74592548a7d99ef615e12d5381
Dylan <delxhq0@gmail.com>
2018-04-09 00:55:42 +0100

    Rewrote the message event

--- events/message.js
+++ events/message.js
@@ -1,16 +1,15 @@
 const config = require('../config.json');
 module.exports = message => {
-    let funo = message.client;
+    funo = message.client;
     if (message.author.bot) return undefined;
-    if (!message.content.startsWith(config.prefix)) return undefined;
-    let command = message.content.split(' ')[0].slice(config.prefix.length);
-    let params = message.content.split(' ').slice(1);
-    let cmd;
-    if (funo.command.has(command)) {
-        cmd = funo.commands.get(command);
-    } else if (funo.aliases.has(command)) {
-        cmd = funo.coammnds.get(funo.aliases.get(coammnd));
-    }
-    if (cmd) cmd.run(funo, message, params);
+    if (message.channel.type === 'dm') return undefined;
 
+    let messageArray = message.content.split(/\s+/g);
+    let command = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if (!command.startsWith(config.prefix)) return undefined;
+
+    let cmd = funo.commands.get(command.slice(config.prefix.length));
+    if (cmd) cmd.run(funo, message, args);
 };
