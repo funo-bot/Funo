fd2c1d391082239b1c6ea7d5b6fb04ee23ce911a
Dylan <delxhq0@gmail.com>
2018-05-05 17:49:18 +0100

    Clean up main file

--- main.js
+++ main.js
@@ -1,34 +1,9 @@
-const Discord   = require('discord.js');
-const config    = require('./config.json');
-const fs        = require('fs');
-const moment    = require('moment');
+const Discord = require('discord.js');
+const config = require('./config.json');
 
-const funo      = new Discord.Client();
-
-const logger    = require('./functions/terminal');
+const funo = new Discord.Client();
 
 require('./util/eventLoader')(funo);
-
-funo.commands     = new Discord.Collection();
-
-fs.readdir('./commands/', (err, files) => {
-    if (err) logger(funo, err);
-
-    let jsfiles = files.filter(f => f.split('.').pop() === 'js');
-    if (jsfiles.length <= 0) {
-        logger(funo, 'No commands to load!');
-        return undefined;
-    }
-
-    logger(funo, `Loading ${jsfiles.length} commands...`);
-
-    jsfiles.forEach((f, i) => {
-        let props = require(`./commands/${f}`);
-        funo.commands.set(props.help.name, props);
-        logger(funo, `${f} command loaded!`);
-    });
-
-    logger(funo, 'All commands loaded!');
-});
+require('./util/commandLoader')(funo);
 
 funo.login(config.token);
