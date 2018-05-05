b0147de319f964843d9b74d53c97a709d19559c4
Dylan <delxhq0@gmail.com>
2018-05-05 17:49:54 +0100

    Moved the command loader to its own file

---
+++ util/commandLoader.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const logger = require('../functions/terminal');
+const fs = require('fs');
+module.exports = funo => {
+
+  funo.commands = new Discord.Collection();
+
+  fs.readdir('./commands/', (err, files) => {
+    if (err) logger(funo, err);
+
+    let jsfiles = files.filter(f => f.split('.').pop() === 'js');
+    if (jsfiles.length <= 0) {
+      logger(funo, 'No commands to load!');
+      return undefined;
+    }
+    logger(funo, `Loading ${jsfiles.length} commands...`);
+
+    jsfiles.forEach((f, i) => {
+      let props = require(`../commands/${f}`);
+      funo.commands.set(props.help.name, props);
+      logger(funo, `${f} command loaded!`);
+    });
+
+    logger(funo, 'All commands loaded!');
+  });
+}
