f8553eb0c50fef9671045c34220d9ad35c866254
Dylan <delxhq0@gmail.com>
2018-05-06 19:02:53 +0100

    Added space so colours are shown correctly

--- functions/terminal.js
+++ functions/terminal.js
@@ -1,5 +1,5 @@
 const moment = require('moment');
 const chalk = require('chalk');
 module.exports = async (funo, message) => {
-    console.log(chalk.cyan("["+ moment().format('HH:mm:ss') + "] ") + chalk.white(message));
+    console.log(chalk.bgBlue("["+ moment().format('HH:mm:ss') + "]") + chalk.white("", message));
 };
