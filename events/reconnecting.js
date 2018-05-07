5f12224bc0ad442bc4511f20188d5edd23987e01
Dylan <delxhq0@gmail.com>
2018-05-07 15:55:52 +0100

    Reconnecting event

---
+++ events/reconnecting.js
@@ -0,0 +1,5 @@
+const logger = require('../util/logger');
+module.exports = funo => {
+  logger(funo, 'Reconnecting to the Discord API...').then(() => logger(funo, 'Successfully reconnected to the Discord API!'));
+  }
+}
