9f53bfec550f9e9d73ec5a9f4725fdb8bd3b7e4b
Dylan <delxhq0@gmail.com>
2018-05-10 14:46:32 +0100

    Update status message

--- functions/setStatusMessage.js
+++ functions/setStatusMessage.js
@@ -1,22 +1,24 @@
-const Discord   = require('discord.js');
-const logger    = require('../functions/terminal');
+const Discord = require('discord.js');
+
+const logger = require('../util/logger');
 
 module.exports = async (funo) => {
     funo.user.setActivity('Loading...');
     setInterval(function () {
         var activitys = {
-            "LISTENING": `to ${funo.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString()} users!`,
-            "WATCHING": `over ${funo.guilds.size} servers`,
-            "PLAYING": `with updates!`
+            'LISTENING': `to ${funo.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString()} users!`,
+            'PLAYING': `in ${funo.guilds.size} servers`,
+            'PLAYING': `with updates!`,
+            'WATCHING': `https://funo.host`
         }
         var types = [
-            "WATCHING",
-            "LISTENING",
-            "PLAYING"
+            'WATCHING',
+            'LISTENING',
+            'PLAYING'
         ];
-        var type = types[Math.floor(Math.random() * types.length)]
+        var type = types[Math.floor(Math.random() * types.length)];
         var name = activitys[type]
-        funo.user.setActivity(name, { type: type })
-        logger(funo, `Status message set to {${type}} {${name}}`)
-    }, 30000)
+        funo.user.setActivity(name, { type: type });
+        logger(funo, `Status message set to {${type}} {${name}}`);
+    }, 30000);
 }
