9f7f20b2e079298dc208e2b71cb9cfa60a32eea8
Dylan <delxhq0@gmail.com>
2018-08-23 03:52:47 +0100

    I some how forgot add that 😆

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -3,5 +3,5 @@ const chalk = require('chalk');
 
 module.exports = guild => {
     funo = guild.client;
-    logger(funo, chalk.green('Added to new guild!' + guild.name + ', with ' + guild.members.size + '!'));
+    logger(funo, chalk.green('Added to new guild!' + guild.name + ', with ' + guild.members.size + 'members!'));
 }
