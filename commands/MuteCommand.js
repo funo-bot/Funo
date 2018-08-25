88504573658cda651ea7dc7ffe7916d9f3329449
Dylan <delxhq0@gmail.com>
2018-08-25 05:56:34 +0100

    Mute command!

---
+++ commands/MuteCommand.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js');
+const error = require('../util/errors');
+const logger = require('../util/logger');
+
+module.exports.run = async (funo, message, args) => {
+    if (!message.member.hasPermission('MANAGE_MESSAGES')) return error.noPermission(message, 'MANAGE_MESSAGES');
+    var toMute = message.guild.member(message.mentions.users.first()) || message.guild.member(args[0]);
+    if (!toMute) return error.noArgs(message);
+
+    if (toMute.id === message.author.id) return error.useOnSelf(message);
+    if (toMute.highestRole.position >= message.member.highestRole.position) return error.userHigherRole(message);
+
+    var role = message.guild.roles.find(r => r.name === 'Funo Muted');
+    if (!role) {
+        try {
+            role = await message.guild.createRole({
+                name: 'Funo Muted',
+                color: '#e63900',
+                permissions: []
+            });
+            message.guild.channels.forEach(async(channel, id) => {
+                await channel.overwritePermissions(role, {
+                    SEND_MESSAGES: false,
+                    ADD_REACTIONS: false,
+                });
+            });
+        } catch (e) {
+            logger.error(e.stack);
+        }
+    }
+    if (toMute.roles.has(role)) message.channel.send(new Discord.RichEmbed().setColor('RED').setAuthor(message.author.username).setDescription(toMute + ' has already been muted!'));
+    await toMute.addRole(role).then(() => message.channel.send(new Discord.RichEmbed().setColor('GREEN').setAuthor(message.author.username).setDescription(toMute + ' has been muted!')));
+}
+
+module.exports.help = {
+    command: "Mute",
+    name: "mute"
+}
--- util/errors.js
+++ util/errors.js
@@ -17,3 +17,21 @@ module.exports.noArgs = (message) => {
 
     message.channel.send({ embed });
 }
+
+module.exports.useOnSelf = (message) => {
+    var embed = new Discord.RichEmbed()
+    .setColor('RED')
+    .setAuthor(message.author.username)
+    .setDescription('⛔ You cannot use that command on yourself!');
+
+    message.channel.send({ embed });
+}
+
+module.exports.userHigherRole = (message) => {
+    var embed = new Discord.RichEmbed()
+    .setColor('RED')
+    .setAuthor(message.author.username)
+    .setDescription('⛔ ' + message.mentions.users.first() + '\'s role is higher than yours!');
+
+    message.channel.send({ embed });
+}
