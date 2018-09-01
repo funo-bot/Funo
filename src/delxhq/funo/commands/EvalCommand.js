6883d5ce9fed5910c22eecfeb1fb2682bb43ef8a
ZeSpB <delxhq0@gmail.com>
2018-09-01 23:51:37 +0100

    Eval command!

---
+++ src/delxhq/funo/commands/EvalCommand.js
@@ -0,0 +1,36 @@
+const os = require('os');
+const Discord = require('discord.js');
+module.exports.run = async (funo, message, args) => {
+    if (message.author.id !== "462407582284513280") return;
+    if (args1 == "client.token" || args1 == "token") return;
+    try {
+        const code = args.slice(1).join(' ');
+        var evaled = eval(code);
+
+        if (typeof evaled !== "string")
+            evaled = require("util").inspect(evaled);
+
+        message.channel.send(new Discord.RichEmbed()
+            .setTitle('**:white_check_mark: Success:**')
+            .setColor('0x55FF00')
+            .addField('📥 **Input:**', `\`\`\`xl\n${code}\n\`\`\``)
+            .addField('📤 **Output:**', `\`\`\`xl\n${clean(evaled)}\n\`\`\``)
+            .setFooter('© Funo Eval', funo.user.avatarURL));
+
+    } catch (err) {
+        message.channel.send(new Discord.RichEmbed()
+            .setTitle('**:x:ERROR:**')
+            .setColor('0xFF0000')
+            .addField('📥 **Input:**', `\`\`\`xl\n${args1.join(" ")}\n\`\`\``)
+            .addField('📤 **Output:**', `\`\`\`xl\n${clean(err)}\n\`\`\``)
+            .setFooter('© Funo | Eval', funo.user.avatarURL));
+    }
+}
+
+function clean(text) {
+    if (typeof (text) === "string") return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203)); else return text;
+}
+module.exports.help = {
+    command: 'Eval',
+    name: "eval"
+}
--- src/delxhq/funo/functions/setStatusMessage.js
+++ src/delxhq/funo/functions/setStatusMessage.js
@@ -2,12 +2,13 @@ module.exports = async (funo) => {
     setInterval(function () {
         var messages = [
             'Coming soon!',
-            'Runs on Ubuntu 18.10!',
+            'Runs on Ubuntu!',
             '2GB of ram 😛',
             'I love you, you love me! ❤',
             'Loving ' + funo.guilds.size + ' servers!',
             'making ' + funo.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ' people happy!',
-            'Come play with me 😄'
+            'Come play with me 😄',
+            'Written in JavaScript!'
         ];
         funo.user.setActivity(messages[Math.floor(Math.random() * messages.length)]);
     }, 30 * 1000);
