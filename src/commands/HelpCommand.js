943c57652d2d4bb1146801ed8064a0e877bd7479
Dylan <delxhq@funo.network>
2019-01-26 09:05:27 +0000

    Change Discord server invite link.

--- src/commands/HelpCommand.js
+++ src/commands/HelpCommand.js
@@ -4,7 +4,7 @@ module.exports.run = (funo, message) => {
 
   const commandNames = Array.from(funo.commands.keys());
   const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-  const commandList = `\`\`\`asciidoc\n= Command List =\n\n${funo.commands.map(cmd => `${config.prefix}${cmd.help.name}${' '.repeat(longest - cmd.help.name.length)} = ${cmd.help.description}`).join('\n')}\n\n= Join our official support Discord! =\nhttps://discord.gg/XtUnRbr\`\`\``
+  const commandList = `\`\`\`asciidoc\n= Command List =\n\n${funo.commands.map(cmd => `${config.prefix}${cmd.help.name}${' '.repeat(longest - cmd.help.name.length)} = ${cmd.help.description}`).join('\n')}\n\n= Join our official support Discord! =\nhttps://discord.gg/e4QcD8Q\`\`\``
 
   message.channel.send(commandList)
 }
