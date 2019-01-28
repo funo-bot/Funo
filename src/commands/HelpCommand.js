bd211ec4ff584a2de5bdc1257354a001e17108c8
Dylan Ogle <delxhq@funo.network>
2019-01-28 16:48:11 +0000

    Revert "Help command rewrite. Still needs worked on."
    This reverts commit e54c6f35ebc7dfce46615e9143474f4ac96594fa.

--- src/commands/HelpCommand.js
+++ src/commands/HelpCommand.js
@@ -1,28 +1,12 @@
 const config = require('../../config.json')
-const Discord = require('discord.js')
 
 module.exports.run = (funo, message) => {
 
-  const max = 5
-  let i = 0
+  /* Rewrite this */
 
-  const funoCommands = Array.from(funo.commands.entries())
+  const commandList = `**Command List:**\n\n${funo.commands.map(cmd => `**${cmd.help.name}** - ${cmd.help.description}`).join('\n')}\n\n[Current prefix is \`${config.prefix}\`]\n\`[To execute a command use ${config.prefix}<command>]\`\n\nJoin our official support Discord if you have any issues with the bot!\nhttps://discord.gg/e4QcD8Q`
 
-  while (i < funoCommands.length) {
-    const cmds = funoCommands.slice(i, i + max)
-    const commandList = new Discord.RichEmbed()
-    
-    cmds.forEach(cmd => commandList.addField(cmd[1].help.name, cmd[1].help.description))
-
-    commandList.addField('Information', '[Discord](https://discord.gg/e4QcD8Q) | [Github](https://github.com/DelxHQ/Funo)')
-    commandList.setTitle('Funo Command List')
-    commandList.setColor('#bf80ff')
-
-    message.channel.send(commandList)
-    console.log('Help command ran!')
-
-    i += cmds.length
-  }
+  message.channel.send(commandList)
 }
 
 module.exports.help = {
