dbeef17c76d1e684ed7772d0636c16ef40f48dfa
Dylan Ogle <delxhq@funo.network>
2019-02-09 04:54:32 +0000

    Get server's prefix from Array

--- src/commands/HelpCommand.js
+++ src/commands/HelpCommand.js
@@ -3,8 +3,8 @@ const config = require('../../config.json')
 module.exports.run = (funo, message) => {
 
   /* Rewrite this */
-
-  const commandList = `**Command List:**\n\n${funo.commands.map(cmd => `**${cmd.help.name}** - ${cmd.help.description}`).join('\n')}\n\n[Current prefix is \`${config.prefix}\`]\n\`[To execute a command use ${config.prefix}<command>]\`\n\nJoin our official support Discord if you have any issues with the bot!\nhttps://discord.gg/e4QcD8Q`
+  const guildConf = funo.settings.ensure(message.guild.id, funo.defaultSettings);
+  const commandList = `**Command List:**\n\n${funo.commands.map(cmd => `**${cmd.help.name}** - ${cmd.help.description}`).join('\n')}\n\n[Current prefix is \`${guildConf.prefix}\`]\n\`[To execute a command use ${guildConf.prefix}<command>]\`\n\nJoin our official support Discord if you have any issues with the bot!\nhttps://discord.gg/e4QcD8Q`
 
   message.channel.send(commandList)
 }
