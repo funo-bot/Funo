dd7fe1ece203406790a81441904c8c47b3207a50
Dylan Ogle <delxhq@funo.network>
2019-02-10 01:22:06 +0000

    Revert to old mute command (Will be updated soon.)

--- src/commands/MuteCommand.js
+++ src/commands/MuteCommand.js
@@ -1,61 +1,42 @@
-const Discord = require('discord.js');
-const logger = require('../util/Logger');
+module.exports.run = async (bot, message, args) => {
+  //Check if command executor has the right permissions to do this command. 
+  if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.sendMessage("You do not have the manage messages permission.")
+  //Get the mentioned user, return if there is none.
+  let toMute = message.guild.member(message.mentions.users.first()) || message.guild.member(args[0]);
+  if (!toMute) return message.channel.send("You did not specify a user to mute!");
 
-module.exports.run = async (message, args) => {
-  const toMute = message.guild.member(message.mentions.users.first()) || message.guild.member(args[0]);
+  if (toMute.id === message.author.id) return message.channel.sendMessage("You cannot mute yourself.");
+  if (toMute.highestRole.position >= message.member.highestRole.position) return message.channel.sendMessage("You cannot mute a member who is higher or has the same role as you.");
 
-  if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send(new Discord.RichEmbed()
-    .setDescription('You lack the `MANAGE_MESSAGES` permisson.')
-    .setColor('RED')
-  )
-
-  if (!toMute) return message.channel.send(new Discord.RichEmbed()
-    .setDescription('You provide someone to unmute.')
-    .setColor('RED')
-  )
-
-  if (toMute.id === message.author.id) return message.channel.send(new Discord.RichEmbed()
-    .setDescription('You cannot mute yourself!')
-    .setColor('RED')
-  )
-
-  if (toMute.highestRole.position > message.member.highestRole.position) return message.channel.send(new Discord.RichEmbed()
-    .setDescription('You cannot mute someone who has a higher role than you!')
-    .setColor('RED')
-  )
-
-  var role = message.guild.roles.find(r => r.name === 'Funo Muted');
+  let role = message.guild.roles.find(r => r.name === "Funo Muted")
   if (!role) {
     try {
       role = await message.guild.createRole({
-        name: 'Funo Muted',
-        color: '#e63900',
+        name: "Funo Muted",
+        color: "#e63900",
         permissions: []
       });
+
       message.guild.channels.forEach(async (channel, id) => {
         await channel.overwritePermissions(role, {
           SEND_MESSAGES: false,
-          ADD_REACTIONS: false,
+          ADD_REACTIONS: false
+
         });
       });
     } catch (e) {
-      logger.error(e.stack);
+      console.log(e.stack);
     }
   }
 
-  if (toMute.roles.has(role.id)) return message.channel.send(new Discord.RichEmbed()
-    .setColor('RED')
-    .setDescription('😕 ' + toMute + ' has already been muted!')
-  )
-  
-  await toMute.addRole(role).then(() => message.channel.send(new Discord.RichEmbed()
-    .setColor('GREEN')
-    .setDescription('😄 ' + toMute + ' has been muted!')
-  ))
+  if (toMute.roles.has(role.id)) return message.channel.sendMessage("That user has already been muted!");
+
+  await toMute.addRole(role).then()
+  message.channel.send(args[0] + " has been muted");
+
 }
 
 module.exports.help = {
-  command: "Mute",
   name: "mute",
-  description: "Mute a user in your server"
+  description: "Mute a user in your Discord server"
 }
