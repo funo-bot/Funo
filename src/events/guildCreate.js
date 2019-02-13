ba4dcab4c2f6b6207a4954af3d82e25868eb627b
Dylan Ogle <delxhq@funo.network>
2019-02-13 21:15:17 +0000

    Send an embed to logging channel when we get added or removed from a guild.

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -1,5 +1,19 @@
+const Discord = require('discord.js')
 const logger = require('../util/Logger');
 
 module.exports = guild => {
   logger.info(`Added to new guild ${guild.name}, with ${guild.members.size} members!`);
+
+  funo.guilds.get(config.logServerID).channels.find("name", config.logChannelName).send(new Discord.RichEmbed()
+    .setThumbnail(guild.iconURL)
+    .setTitle("Added to new guild")
+    .setColor('BLUE')
+    .addField("Name:", guild.name, true)
+    .addField("ID:", guild.id, true)
+    .addField("Owned by:", `${guild.owner.user.username}#${guild.owner.user.discriminator}`, true)
+    .addField("Owner's ID", guild.owner.user.id, true)
+    .addField("Members:", guild.members.size, true)
+    .addField("Channels:", guild.channels.size, true)
+    .addField("Roles:", guild.roles.size, true)
+  )
 }
--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -2,7 +2,20 @@ const logger = require('../util/Logger');
 
 module.exports = guild => {
   funo = guild.client
-  
+
   funo.settings.delete(guild.id);
   logger.info(`Removed from ${guild.name}, with ${guild.members.size} members!`);
+
+  funo.guilds.get(config.logServerID).channels.find("name", config.logChannelName).send(new Discord.RichEmbed()
+    .setThumbnail(guild.iconURL)
+    .setTitle("Removed from a guild")
+    .setColor('RED')
+    .addField("Name:", guild.name, true)
+    .addField("ID:", guild.id, true)
+    .addField("Owned by:", `${guild.owner.user.username}#${guild.owner.user.discriminator}`, true)
+    .addField("Owner's ID", guild.owner.user.id, true)
+    .addField("Members:", guild.members.size, true)
+    .addField("Channels:", guild.channels.size, true)
+    .addField("Roles:", guild.roles.size, true)
+  )
 }
