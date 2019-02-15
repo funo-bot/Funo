8604480fa20d193ea32053f301d863568c97fc56
Dylan Ogle <delxhq@funo.network>
2019-02-15 23:42:13 +0000

    Send an embed when we get a vote.

--- src/util/DblHelper.js
+++ src/util/DblHelper.js
@@ -1,9 +1,15 @@
 const DBL = require('dblapi.js');
+const Discord = require('discord.js')
 const config = require('../../config.json')
 
 module.exports = funo => {
   const dbl = new DBL(config.DBLAPI_token, { webhookPort: 5000, webhookAuth: config.webHookAuth });
 
   dbl.webhook.on('ready', hook => console.log(`Webhook running at http://${hook.hostname}:${hook.port}${hook.path}`))
-  dbl.webhook.on('vote', vote => console.log(`User with ID ${vote.user} just voted!`))
+  dbl.webhook.on('vote', vote => {
+    funo.guilds.get(config.logServerID).channels.find("name", config.logChannelName).send(new Discord.RichEmbed()
+      .setDescription(`User with ID ${vote.username} just voted!`)
+      .setColor('BLUE')
+    )
+  })
 }
