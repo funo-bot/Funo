1ca53af86116938f020ad510d79754356da71cd3
Dylan Ogle <delxhq@funo.network>
2019-02-16 00:41:43 +0000

    This will probably break.

--- src/util/DblHelper.js
+++ src/util/DblHelper.js
@@ -1,19 +1,22 @@
 const DBL = require('dblapi.js');
 const Discord = require('discord.js')
 const config = require('../../config.json')
-const logger = require('./Logger')
 
 module.exports = async funo => {
   const dbl = new DBL(config.DBLAPI_KEY, { webhookPort: 5000, webhookAuth: config.webHookAuth });
  
   dbl.webhook.on('vote', async vote => {
     funo.guilds.get(config.logServerID).channels.find("name", config.logChannelName).send(new Discord.RichEmbed()
-      .setDescription(`User ${await getUsername(vote)} just voted!`)
+      .setThumbnail(await getVoteUser(vote).avatar)
+      .setDescription(`User ${await getVoteUser(vote).username}#${await getVoteUser(vote).descriminator} just voted!`)
       .setColor('BLUE')
+      .setTimestamp()
     )
+
+    console.log()
   })
 
-  async function getUsername(vote) {
-    return new Promise(async resolve => resolve((await dbl.getUser(vote.user)).username))
-  }
+  async function getVoteUser(vote) {
+    return new Promise(async resolve => resolve(await dbl.getUser(vote.user)))
+}
 }
