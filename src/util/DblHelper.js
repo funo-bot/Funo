23f4203132f50fa23eca3ae1dc84666ef33bf036
Dylan Ogle <delxhq@funo.network>
2019-02-16 00:05:17 +0000

    Get username from ID

--- src/util/DblHelper.js
+++ src/util/DblHelper.js
@@ -1,15 +1,22 @@
 const DBL = require('dblapi.js');
 const Discord = require('discord.js')
 const config = require('../../config.json')
+const logger = require('./Logger')
 
 module.exports = funo => {
   const dbl = new DBL(config.DBLAPI_token, { webhookPort: 5000, webhookAuth: config.webHookAuth });
 
-  dbl.webhook.on('ready', hook => console.log(`Webhook running at http://${hook.hostname}:${hook.port}${hook.path}`))
+  dbl.webhook.on('ready', hook => logger.info(`Webhook running at http://${hook.hostname}:${hook.port}${hook.path}`))
+ 
+ 
   dbl.webhook.on('vote', vote => {
     funo.guilds.get(config.logServerID).channels.find("name", config.logChannelName).send(new Discord.RichEmbed()
-      .setDescription(`User with ID ${vote.username} just voted!`)
+      .setDescription(`User ${await getUsername(vote)} just voted!`)
       .setColor('BLUE')
     )
   })
+}
+
+async function getUsername(vote) {
+  return new Promise(resolve => resolve((await dbl.getUser(vote.user)).username))
 }
