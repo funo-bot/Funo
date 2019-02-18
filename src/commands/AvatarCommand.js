b117a344a388f55ccbc29c0d7c948af82c7385ec
Dylan Ogle <delxhq@funo.network>
2019-02-18 21:41:35 +0000

    Implement AvatarCommand.js

---
+++ src/commands/AvatarCommand.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (funo, message, args) => {
+  
+  let m = await message.channel.send(new Discord.RichEmbed()
+    .setDescription('Getting avatar...')
+    .setColor('BLUE')
+  );
+
+  let target = message.mentions.users.first() || message.author;
+
+  await message.channel.send({
+    files: [
+      {
+        attachment: target.displayAvatarURL,
+        name: "avatar.png"
+      }
+    ]
+  });
+
+  m.delete();
+}
+
+module.exports.help = {
+  name: "avatar"
+};
