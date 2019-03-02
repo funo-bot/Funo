3300c4527b9b46b98cdb48e57e5a4087f63d5dd3
Suremeo <me@suremeo.rip>
2019-03-01 19:46:07 -0600

    made memberflow over a 60 minute basis

--- src/util/addToStat.js
+++ src/util/addToStat.js
@@ -5,19 +5,17 @@ Date: 2/28/2019, 11:53 PM
 
 module.exports = async (bot, guild) => {
     var config = await bot.stats.ensure(guild.id, bot.defaultStats)
-
     if (!config.memberArray) config["memberArray"] = []
     if (!config.dateArray) config["dateArray"] = []
     var d = new Date();
-    var date = await d.getMonth() + 1 + "/" + await d.getDate()
-
-    if (config.dateArray[config.dateArray.length - 1] != date) {
+    var date = await d.getMinutes().toString().split("")[0]
+    if (config.dateArray[config.dateArray.length -1] != date) {
         config.dateArray.push(date)
         config.memberArray.push(guild.members.size)
     } else {
         config.memberArray[config.memberArray.length -1] = guild.members.size
     }
 
-    bot.settings.set(guild.id, config.dateArray, "datesArray")
+    bot.settings.set(guild.id, config.dateArray, "dateArray")
     bot.settings.set(guild.id, config.memberArray, "memberArray")
 }
