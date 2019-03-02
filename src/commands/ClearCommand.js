439dce6ed69e21dc52d13a8db273eb3c61c69388
greyliances <greyliances1@outlook.de>
2019-03-02 15:57:58 +0100

    Better Clear Command
    Added support for deleting messages by a tagged user, added checks for the amount of messages that are supposed to be deleted. Fixed the responses and the timings.
    
    New syntax:
    <prefix><clear | alias> <number> [mention]

--- src/commands/ClearCommand.js
+++ src/commands/ClearCommand.js
@@ -1,30 +1,63 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+  message.delete().catch();
 
   if (!message.member.hasPermission("MANAGE_MESSAGES")) {
-    return message.channel.send(new Discord.RichEmbed()
-      .setDescription("You lack the `MANAGE_MESSAGES` permisson.")
-      .setColor("RED")
-    );
+    return message.channel
+      .send(
+        new Discord.RichEmbed()
+          .setDescription("You lack the `MANAGE_MESSAGES` permisson.")
+          .setColor("RED")
+      )
+      .then(msg => msg.delete(7000));
+  }
+
+  if (isNaN(args[0]) || args[0] >= 100 || args[0] <= 0) {
+    return message.channel
+      .send(
+        new Discord.RichEmbed()
+          .setDescription(
+            "Make sure to provide a valid number between **1** and **100** as first argument."
+          )
+          .setColor("RED")
+      )
+      .then(msg => msg.delete(7000));
   }
 
   const fetched = await message.channel.fetchMessages({
     limit: args[0]
   });
 
-  message.channel.send(`Deleting ${fetched.size} messages...`).then((msg) => msg.delete(3000));
-  await message.channel.bulkDelete(fetched).catch((error) => message.channel.send(`Error: ${error}`));
-  message.channel.send(`Deleted ${fetched.size} successfully!`).then((msg) => msg.delete(3000));
+  const fetchedSize = fetched.size;
+  const mentioned = message.mentions.users.first();
 
+  message.channel
+    .send(`Deleting ${fetchedSize} message${fetchedSize !== 1 ? "s" : ""}...`)
+    .then(msg => msg.delete(7000));
+  await message.channel
+    .bulkDelete(
+      mentioned
+        ? fetched.filter(msg => msg.author.id === mentioned.id)
+        : fetched
+    )
+    .catch(error => message.channel.send(`Error: ${error}`));
+  message.channel
+    .send(
+      `${
+        mentioned
+          ? fetched.filter(msg => msg.author.id === mentioned.id).size
+          : fetchedSize
+      } out of ${fetchedSize} message${
+        fetchedSize !== 1 ? "s" : ""
+      } matched the given criteria!`
+    )
+    .then(msg => msg.delete(7000));
 };
 
 module.exports.help = {
   name: "clear",
   category: "moderation",
   description: "Delete X ammount of message in a Discord server.",
-  aliases: [
-    "prune",
-    "delete"
-  ]
+  aliases: ["prune", "delete"]
 };
