a01d692979ed9130a810a5d140e643e08c62deeb
Dylan Ogle <delxhq@funo.network>
2019-03-15 17:00:35 +0000

    Leave channel when stop is called.

--- src/commands/StopCommand.js
+++ src/commands/StopCommand.js
@@ -6,28 +6,27 @@ Date: 3/2/2019, 5:12 PM
 const Discord = require("discord.js");
 
 module.exports.run = async (funo, message, args) => {
-    if (!funo.musicTasks[message.guild.id]) return message.channel.send(new Discord.RichEmbed()
-        .setDescription(`Nothing is playing right now!`)
-        .setColor("#36393F")
-        .setThumbnail("https://rhythmcadence.carrd.co/assets/images/image01.gif" ||"https://hotemoji.com/images/dl/i/musical-note-emoji-by-twitter.png")
-        .setImage("https://i.imgur.com/BwPBeG9.gif"))
+  if (!funo.musicTasks[message.guild.id]) return message.channel.send(new Discord.RichEmbed()
+    .setDescription(`Nothing is playing right now!`)
+    .setColor("#36393F")
+    .setThumbnail("https://rhythmcadence.carrd.co/assets/images/image01.gif" || "https://hotemoji.com/images/dl/i/musical-note-emoji-by-twitter.png")
+    .setImage("https://i.imgur.com/BwPBeG9.gif"))
 
-    var task = funo.musicTasks[message.guild.id]
-    task.Stop();
-    message.channel.send(new Discord.RichEmbed()
-        .setDescription("**Successfully cleared the queue and stopped the current song**")
-        .setColor("#36393F")
-        .setThumbnail("https://rhythmcadence.carrd.co/assets/images/image01.gif" ||"https://hotemoji.com/images/dl/i/musical-note-emoji-by-twitter.png")
-        .setImage("https://i.imgur.com/BwPBeG9.gif"))
+  var task = funo.musicTasks[message.guild.id]
+  task.Stop();
+  message.channel.send(new Discord.RichEmbed()
+    .setDescription("**Successfully cleared the queue and stopped the current song**")
+    .setColor("#36393F")
+    .setThumbnail("https://rhythmcadence.carrd.co/assets/images/image01.gif" || "https://hotemoji.com/images/dl/i/musical-note-emoji-by-twitter.png")
+    .setImage("https://i.imgur.com/BwPBeG9.gif"))
 }
 
 module.exports.help = {
-    command: "Stop",
-    name: "stop",
-    category: "music",
-    description: "Stops the current song and clears the queue",
-    aliases: [
-        "stop",
-        "no"
-    ]
+  command: "Stop",
+  name: "stop",
+  category: "music",
+  description: "Stops the current song and clears the queue",
+  aliases: [
+    "no"
+  ]
 };
--- src/util/musicHandler.js
+++ src/util/musicHandler.js
@@ -6,106 +6,108 @@ var YTDL = require("ytdl-core")
 const EventEmitter = require('events');
 const Discord = require("discord.js")
 module.exports = class extends EventEmitter {
-    constructor(guild, channel) {
-        super()
+  constructor(guild, channel) {
+    super()
 
-        this.funo = guild.client
-        this.guild = guild
-        this.channel = channel
-        this.queue = []
-        this.init()
+    this.funo = guild.client
+    this.guild = guild
+    this.channel = channel
+    this.queue = []
+    this.init()
+  }
+
+  Skip() {
+    if (this.dispatcher != null) {
+      this.dispatcher.end()
     }
-
-    Skip() {
-        if (this.dispatcher !=null) {
-            this.dispatcher.end()
-        }
+  }
+  Stop() {
+    this.queue = [null]
+    if (this.dispatcher != null) {
+      this.dispatcher.end()
+      this.channel.leave()
     }
-    Stop() {
-        this.queue = [null]
-        if (this.dispatcher != null) {
-            this.dispatcher.end()
-        }
+  }
+  set Channel(channel) {
+    this.channel = channel
+  }
+
+  get Channel() {
+    return this.channel
+  }
+
+  get nowPlaying() {
+    if (!this.queue[0]) return false
+    var minutes = "0" + Math.floor(this.queue[0].data.length_seconds / 60);
+    var seconds = "0" + (this.queue[0].data.length_seconds - minutes * 60);
+
+    var minutestwo = "0" + Math.floor(this.dispatcher.time / 1000 / 60);
+    var secondstwo = "0" + (Number(`${this.dispatcher.time / 1000}`.split(".")[0]) - minutestwo * 60)
+
+    return {
+      "url": this.queue[0].url,
+      "data": this.queue[0].data,
+      "song_length": minutes.substr(-2) + ":" + seconds.substr(-2),
+      "song_thru": minutestwo.substr(-2) + ":" + secondstwo.substr(-2),
     }
-    set Channel(channel) {
-        this.channel = channel
-    }
+  }
 
-    get Channel() {
-        return this.channel
-    }
-
-    get nowPlaying() {
-        if (!this.queue[0]) return false
-        var minutes = "0" + Math.floor(this.queue[0].data.length_seconds / 60);
-        var seconds = "0" + (this.queue[0].data.length_seconds - minutes * 60);
-
-        var minutestwo = "0" + Math.floor(this.dispatcher.time / 1000 / 60);
-        var secondstwo = "0" + (Number(`${this.dispatcher.time / 1000}`.split(".")[0]) - minutestwo * 60)
-
-        return {
-            "url": this.queue[0].url,
-            "data": this.queue[0].data,
-            "song_length": minutes.substr(-2) + ":" + seconds.substr(-2),
-            "song_thru": minutestwo.substr(-2) + ":" + secondstwo.substr(-2),
-        }
-    }
-
-    async _playNext() {
-        this.queue.shift();
-        if (this.queue[0]) {
-            var minutes = "0" + Math.floor(this.queue[0].data.length_seconds / 60);
-            var seconds = "0" + (this.queue[0].data.length_seconds - minutes * 60);
-            this.connection = await this.channel.join()
-            this.dispatcher = this.connection.playStream(YTDL(this.queue[0].url, {filter: "audioonly"}))
-            this.dispatcher.on("end", async () => {
-                this.dispatcher = null
-                this._playNext()
-            })
-        } else {
-            setTimeout(async () => {
-                try {
-                    if (this.dispatcher==null) this.channel.leave()
-                } catch(err){}
-            },300000)
-        }
-    }
-
-    async addSong(song_url, textchannel, cb) {
+  async _playNext() {
+    this.queue.shift();
+    if (this.queue[0]) {
+      var minutes = "0" + Math.floor(this.queue[0].data.length_seconds / 60);
+      var seconds = "0" + (this.queue[0].data.length_seconds - minutes * 60);
+      this.connection = await this.channel.join()
+      this.dispatcher = this.connection.playStream(YTDL(this.queue[0].url, { filter: "audioonly" }))
+      this.dispatcher.on("end", async () => {
+        this.dispatcher = null
+        this._playNext()
+      })
+    } else {
+      setTimeout(async () => {
         try {
-            this.emit("addSong", {
-                data: await YTDL.getInfo(song_url),
-                cb: cb,
-                channel: textchannel.id
-            })
-        } catch(err) {
-            cb(err)
-        }
-        // console.log(await YTDL.getInfo(song_url))
+          if (this.dispatcher == null) this.channel.leave()
+        } catch (err) { }
+      }, 300000)
     }
+  }
 
-    async init() {
-        this.connection = await this.channel.join()
-        this.on("addSong", async (data, textchannel, cb) => {
-                let song_data = data.data
-                var minutes = "0" + Math.floor(song_data.length_seconds / 60);
-                var seconds = "0" + (song_data.length_seconds - minutes * 60);
-            if (this.queue[0]) {
-                try {
-                    this.queue.push({url: song_data.video_url, data: song_data, cb: cb, channel: textchannel, time: minutes.substr(-2) + ":" + seconds.substr(-2)})
-                    data.cb(`<:t:550084023775395840><:t:550084023775395840><:t:550084023775395840><:t:550084023775395840>**Added to queue**\n**[${song_data.title.replace(/\[/g, '').replace(/\]/g, '').substr(0, 43)}...](${song_data.video_url})**\n<:t:550084023775395840><:t:550084023775395840><:t:550084023775395840><:t:550084023775395840> <:t:550084023775395840>\`${minutes.substr(-2) + ":" + seconds.substr(-2)}\``)
-                } catch(err) {data.cb(err)}
-            } else {
-                try{
-                    this.connection = await this.channel.join()
-                    this.dispatcher = this.connection.playStream(YTDL(song_data.video_url, {filter: "audioonly"}))
-                    this.dispatcher.on("end", async () => {
-                        this.dispatcher = null
-                        this._playNext()
-                    })
-                    this.queue.push({url: song_data.video_url, data: song_data, cb: cb, channel: textchannel, time: minutes.substr(-2) + ":" + seconds.substr(-2)})
-                    data.cb(`<:t:550084023775395840> <:t:550084023775395840> <:t:550084023775395840><:t:550084023775395840>**Now playing**\n**[${song_data.title.replace(/\[/g, '').replace(/\]/g, '').substr(0, 43)}...](${song_data.video_url})**\n<:t:550084023775395840><:t:550084023775395840><:t:550084023775395840><:t:550084023775395840> <:t:550084023775395840>\`${minutes.substr(-2) + ":" + seconds.substr(-2)}\``)
-                } catch(err) { data.cb(err) }}
-        })
+  async addSong(song_url, textchannel, cb) {
+    try {
+      this.emit("addSong", {
+        data: await YTDL.getInfo(song_url),
+        cb: cb,
+        channel: textchannel.id
+      })
+    } catch (err) {
+      cb(err)
     }
+    // console.log(await YTDL.getInfo(song_url))
+  }
+
+  async init() {
+    this.connection = await this.channel.join()
+    this.on("addSong", async (data, textchannel, cb) => {
+      let song_data = data.data
+      var minutes = "0" + Math.floor(song_data.length_seconds / 60);
+      var seconds = "0" + (song_data.length_seconds - minutes * 60);
+      if (this.queue[0]) {
+        try {
+          this.queue.push({ url: song_data.video_url, data: song_data, cb: cb, channel: textchannel, time: minutes.substr(-2) + ":" + seconds.substr(-2) })
+          data.cb(`<:t:550084023775395840><:t:550084023775395840><:t:550084023775395840><:t:550084023775395840>**Added to queue**\n**[${song_data.title.replace(/\[/g, '').replace(/\]/g, '').substr(0, 43)}...](${song_data.video_url})**\n<:t:550084023775395840><:t:550084023775395840><:t:550084023775395840><:t:550084023775395840> <:t:550084023775395840>\`${minutes.substr(-2) + ":" + seconds.substr(-2)}\``)
+        } catch (err) { data.cb(err) }
+      } else {
+        try {
+          this.connection = await this.channel.join()
+          this.dispatcher = this.connection.playStream(YTDL(song_data.video_url, { filter: "audioonly" }))
+          this.dispatcher.on("end", async () => {
+            this.dispatcher = null
+            this._playNext()
+          })
+          this.queue.push({ url: song_data.video_url, data: song_data, cb: cb, channel: textchannel, time: minutes.substr(-2) + ":" + seconds.substr(-2) })
+          data.cb(`<:t:550084023775395840> <:t:550084023775395840> <:t:550084023775395840><:t:550084023775395840>**Now playing**\n**[${song_data.title.replace(/\[/g, '').replace(/\]/g, '').substr(0, 43)}...](${song_data.video_url})**\n<:t:550084023775395840><:t:550084023775395840><:t:550084023775395840><:t:550084023775395840> <:t:550084023775395840>\`${minutes.substr(-2) + ":" + seconds.substr(-2)}\``)
+        } catch (err) { data.cb(err) }
+      }
+    })
+  }
 }
