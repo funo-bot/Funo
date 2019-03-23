4319619dba5f2bea5f5f7bc3f7df95cfa3da1e1f
Dylan Ogle <delxhq@funo.network>
2019-03-23 07:16:45 +0000

    Implement ImgurCommand.

---
+++ src/commands/ImgurCommand.js
@@ -0,0 +1,40 @@
+const config = require("../../config.json")
+const Discord = require("discord.js")
+const fetch = require("node-fetch")
+
+module.exports.run = async (funo, message, args) => {
+  const imgurOptions = { 'Authorization': config.IMGUR_CLIENT_ID }
+
+  await fetch('https://api.imgur.com/3/gallery/search/top/0/?q=' + args.join(" "), {
+    headers: {
+      'Authorization': `Client-ID ${imgurOptions.Authorization}`
+    }
+  })
+    .then(res => res.json())
+    .then(json => {
+      if (json['data'] < 1) {
+        return message.channel.send(new Discord.RichEmbed()
+          .setColor("RED")
+          .setDescription("Nothing was found with your search.")
+        );
+      }
+      const reply = json['data'][Math.floor(Math.random() * json['data'].length)]
+
+      message.channel.send(new Discord.RichEmbed()
+        .setColor("BLUE")
+        .setTitle(reply.title)
+        .setURL(reply.link)
+        .setDescription(JSON.stringify(reply.is_album) === "true" ? reply.images[0].description || "No description specifed." : reply.description || "No description specifed.")
+        .setImage(JSON.stringify(reply.is_album) === "true" ? reply.images[0].link : reply.link)
+      );
+
+      console.log(reply)
+    })
+}
+
+module.exports.help = {
+  name: "imgur",
+  category: "fun",
+  description: "Search for images with Imgur",
+  aliases: []
+}
