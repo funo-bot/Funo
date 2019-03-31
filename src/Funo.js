6d7f45e748a7dfe66cac440e4a701734997e285d
Dylan Ogle <delxhq@funo.network>
2019-03-31 02:06:02 +0100

    Refactor

--- src/Funo.js
+++ src/Funo.js
@@ -1,7 +1,5 @@
 const Discord = require("discord.js");
 const Enmap = require("enmap");
-const config = require(process.env.CONFIG_LOCATION || "../config.json");
-const logger = require("./util/Logger");
 
 const funo = new Discord.Client({ disableEveryone: true });
 const start = Date.now();
@@ -9,7 +7,12 @@ const start = Date.now();
 funo.musicTask = require("./util/musicHandler")
 funo.musicTasks = {}
 
-funo.config = config
+funo.config = require(process.env.CONFIG_LOCATION || "../config.json");
+funo.logger = require("./util/Logger");
+
+require("./util/EventLoader")(funo);
+require("./util/CommandLoader")(funo);
+require("./util/DblHelper")(funo);
 
 funo.settings = new Enmap({
   name: "settings",
@@ -29,10 +32,6 @@ funo.defaultSettings = {
   prefix: ".",
   logChannel: "mod-logs"
 };
-
-require("./util/EventLoader")(funo);
-require("./util/CommandLoader")(funo);
-require("./util/DblHelper")(funo);
 
 funo.login(config.token).then(() => {
   const finish = Date.now() - start;
--- src/util/CommandLoader.js
+++ src/util/CommandLoader.js
@@ -1,6 +1,5 @@
-const Discord = require("discord.js");
-const fs = require("fs");
-const logger = require("./Logger");
+const Discord = require('discord.js');
+const fs = require('fs');
 
 module.exports = (funo) => {
   funo.commands = new Discord.Collection();
@@ -8,27 +7,24 @@ module.exports = (funo) => {
 
   fs.readdir(`${__dirname}/../commands`, (e, files) => {
     if (e) {
-      logger.error(e);
+      funo.logger.error(e);
     }
 
-    let commands = files.filter((f) => f.split(".").pop() === "js");
+    const commands = files.filter(f => f.split('.').pop() === 'js');
     if (commands.length <= 0) {
-      logger.info("No commands to load!");
+      funo.logger.info('No commands to load!');
       return;
     }
 
     commands.forEach((cmd) => {
-      var props = require(`../commands/${cmd}`);
+      const props = require(`../commands/${cmd}`);
       if (props.help.disabled) {
-
+        
       } else {
-          funo.commands.set(props.help.name.toLowerCase(), props);
-          props.help.aliases.forEach((alias) =>
-              funo.aliases.set(alias, props.help.name.toLowerCase())
-          )
+        funo.commands.set(props.help.name.toLowerCase(), props);
+        props.help.aliases.forEach(alias => funo.aliases.set(alias, props.help.name.toLowerCase()));
       }
     });
-
-    logger.info("All commands loaded!");
+    funo.logger.info('All commands loaded!');
   });
 };
