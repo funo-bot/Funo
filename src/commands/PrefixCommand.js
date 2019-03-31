96bc0d7c92b33f759f944486feeb2c9018e14488
Dylan Ogle <delxhq@funo.network>
2019-03-31 03:05:33 +0100

    Move permission check.

--- src/commands/PrefixCommand.js
+++ src/commands/PrefixCommand.js
@@ -3,19 +3,19 @@ const Discord = require("discord.js");
 
 module.exports.run = async (funo, message, args) => {
 
-  if (!message.member.hasPermission("MANAGE_GUILD")) {
-    return message.channel.send(new Discord.RichEmbed()
-      .setDescription("You lack the **`MANAGE_GUILD`** permisson.")
-      .setColor("RED")
-    );
-  }
-
   const guildConf = funo.settings.ensure(message.guild.id, funo.defaultSettings);
 
   if(!args.length) {
     return message.channel.send(new Discord.RichEmbed()
       .setDescription(`This server's current prefix is **\`${guildConf.prefix || config.prefix}\`**`)
       .setColor("BLUE")
+    );
+  }
+
+  if (!message.member.hasPermission("MANAGE_GUILD")) {
+    return message.channel.send(new Discord.RichEmbed()
+      .setDescription("You lack the **`MANAGE_GUILD`** permisson.")
+      .setColor("RED")
     );
   }
 
