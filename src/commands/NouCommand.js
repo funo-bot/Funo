68e88b8c67930e948397642044799e92a1eb43a1
Dylan Ogle <delxhq@funo.network>
2019-04-25 00:51:25 +0100

    No U command :P

---
+++ src/commands/NouCommand.js
@@ -0,0 +1,12 @@
+module.exports.run = async (funo, message, args) => {
+  message.channel.send('🇳🅾    🇺')
+}
+
+module.exports.help = {
+  command: "nou",
+  name: "nou",
+  category: "fun",
+  description: "Say NO U to someone",
+  aliases: [],
+  permissions: ['CONNECT', 'SPEAK']
+};
