5abf84cc84d0d6fbb3b9d327bb63433997d2d28a
Dylan Ogle <delxhq@funo.network>
2019-11-05 21:42:55 +0000

    Get rid of events folder.

--- src/events/error.js
+++
@@ -1,5 +0,0 @@
-const logger = require("../util/Logger");
-
-module.exports = (error) => {
-  logger.error("An unknown error has occured. " + error.stack)
-};
--- src/events/ready.js
+++
@@ -1,7 +0,0 @@
-const logger = require("../util/Logger");
-
-module.exports = (funo) => {
-  logger.info(`Logged in with ${funo.guilds.size} servers and ${funo.users.size} users.`);
-
-  funo.user.setPresence({ game: { name: funo.prefix + "help for commands", type: 0 } });
-};
